886f7a40566df76cdac21fb56b12585eee136a12
Umut-Umut <umutsanci25@gmail.com>
2025-06-06 00:02:07 +0300

    v0.0.5

--- scripts/CKeyboard.gd
+++ scripts/CKeyboard.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal update_direction(d)
+
 # Hareket ayarları
 export var walk_speed := 200
 export var run_speed := 350
@@ -10,16 +12,24 @@ export var gravity := 900
 var velocity := Vector2.ZERO
 var jump_count := 0
 const MAX_JUMPS := 2
+var parent : KinematicBody2D
+var input_direction = 0
+
+func _ready():
+	parent = get_parent()
+	if not parent:
+		print_debug("Parent must be a KinematicBody2D")
 
 func _physics_process(delta):
-	var parent = get_parent()  # KinematicBody2D
-	var input_direction := 0
+	input_direction = 0
 
 	# Yatay hareket
 	if Input.is_action_pressed("ui_right"):
-		input_direction += 1
+		input_direction = 1
+		emit_signal("update_direction", input_direction)
 	if Input.is_action_pressed("ui_left"):
-		input_direction -= 1
+		input_direction = -1
+		emit_signal("update_direction", input_direction)
 	
 	var speed = run_speed if Input.is_action_pressed("l_shift") else walk_speed
 	velocity.x = input_direction * speed
---
+++ scripts/Sword.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and not event.echo:
+			match event.scancode:
+				KEY_X:
+					monitoring = true
+		else:
+			if monitoring:
+				monitoring = false
+
+
+
+
+func _on_CKeyboard_update_direction(d):
+	if self.scale.x != d:
+		self.scale.x = d
